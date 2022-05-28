770e0fa9daf62c9e1a303899b047f36bab045243
junbohe <he969199286@gmail.com>
2022-05-27 23:06:59 -0500

    Added controller, view resolver and model.

---
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+public class DemoController {
+    // http://localhost:8080/todo-list/hello
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    // prefix + name + suffix
+    // /WEB0INF/view/welcome.jsp
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("user", "Tim");
+        log.info("model= {}", model);
+        return "welcome";
+    }
+}
