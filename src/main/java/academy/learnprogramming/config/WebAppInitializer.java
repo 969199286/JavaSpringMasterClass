7acaf7900e9e834169d3d5a921b0d9a34b4a6876
junbohe <he969199286@gmail.com>
2022-05-31 16:07:08 -0500

    Able to add, edit, delete and view the todo-list.

--- src/main/java/academy/learnprogramming/config/WebAppInitializer.java
+++ src/main/java/academy/learnprogramming/config/WebAppInitializer.java
@@ -1,9 +1,7 @@
 package academy.learnprogramming.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -12,7 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 @Slf4j
-public class WellAppInitializer implements WebApplicationInitializer {
+public class WebAppInitializer implements WebApplicationInitializer {
     private static final String DISPATCHER_SERVLET_NAME = "dispatcher";
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
